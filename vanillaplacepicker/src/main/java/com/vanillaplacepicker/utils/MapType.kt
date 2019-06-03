8465519917e7443fa07e65e3a936dbf8eedbb614
Malik Motani <mjmotani@gmail.com>
2019-06-03 11:48:30 +0530

    Set GoogleMap default MapType values in VanillaMapType enum

--- vanillaplacepicker/src/main/java/com/vanillaplacepicker/utils/MapType.kt
+++ vanillaplacepicker/src/main/java/com/vanillaplacepicker/utils/MapType.kt
@@ -1,8 +1,11 @@
 package com.vanillaplacepicker.utils
 
+import com.google.android.gms.maps.GoogleMap
+
 enum class MapType(val value: Int) {
-    NORMAL(1),
-    SATELLITE(2),
-    TERRAIN(3),
-    HYBRID(4)
+    NORMAL(GoogleMap.MAP_TYPE_NORMAL),
+    SATELLITE(GoogleMap.MAP_TYPE_SATELLITE),
+    TERRAIN(GoogleMap.MAP_TYPE_TERRAIN),
+    HYBRID(GoogleMap.MAP_TYPE_HYBRID),
+    NONE(GoogleMap.MAP_TYPE_NONE)
 }
